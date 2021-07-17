fe0a12acabeb94d5bc6169b9c08e18e49524bbce
santisan4 <sant97@hotmail.com.ar>
2021-07-16 22:00:33 -0300

    haciendo register

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
 app.get('/', (req,res) =>{
    res.sendFile(path.join(__dirname, './views/index.html'));  // Permite enviar un archivo HTML
 });
 
-app.get('/', (req,res) =>{
-    res.sendFile(path.join(__dirname, './views/register.html'));
- });
- 
-
 app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname, './views')));
 
 app.listen(process.env.PORT || 3000, function(){
     console.log("Servidor corriendo en el puerto 3000");
 })
+
